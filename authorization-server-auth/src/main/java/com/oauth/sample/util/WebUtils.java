dfc0ea65aa74a9c65734656947e50b7024951716
jiangmb <jiangmb@televehicle.com>
2023-07-16 14:39:43 +0800

    [chg]add service

--- authorization-server-auth/src/main/java/com/oauth/sample/util/WebUtils.java
+++ authorization-server-auth/src/main/java/com/oauth/sample/util/WebUtils.java
@@ -1,23 +1,9 @@
-/*
- * Copyright (c) 2020 pig4cloud Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.oauth.sample.util;
 
 import cn.hutool.core.codec.Base64;
-import com.pig4cloud.pig.common.core.exception.CheckedException;
+import com.oauth.sample.exception.CheckedException;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -31,7 +17,6 @@ import org.springframework.web.method.HandlerMethod;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
@@ -152,7 +137,6 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 		return null;
 	}
 
-	@NotNull
 	private static String[] splitClient(String header) {
 		if (header == null || !header.startsWith(BASIC_)) {
 			throw new CheckedException("请求头中client信息为空");
